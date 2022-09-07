350143e01144d12b7bcf51d17dcab731dd6cc3f0
Jaehwany <eejh22@gmail.com>
2022-09-08 01:18:44 +0900

    feat : Swagger 설정 (#8)

--- samals/src/test/java/com/project/samals/SamalsApplicationTests.java
+++ samals/src/test/java/com/project/samals/SamalsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ace.samals;
+package com.project.samals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SamalsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
