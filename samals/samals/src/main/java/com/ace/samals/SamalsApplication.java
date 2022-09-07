ec8788f348b8032f9197e030ad7840353bc4d014
devyoseph <josephdev123@naver.com>
2022-09-07 17:17:38 +0900

    feat : Spring Setting

---
+++ samals/samals/src/main/java/com/ace/samals/SamalsApplication.java
@@ -0,0 +1,13 @@
+package com.ace.samals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamalsApplication.class, args);
+	}
+
+}
---
+++ samals/samals/src/test/java/com/ace/samals/SamalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.samals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
