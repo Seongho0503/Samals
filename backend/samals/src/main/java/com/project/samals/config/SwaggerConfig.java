37fb79b4c3a10f41c936528b9925da6985a9f954
Jaehwany <eejh22@gmail.com>
2022-09-13 00:24:08 +0900

    fix : Swagger basePackage 수정 (#8)

--- backend/samals/src/main/java/com/project/samals/config/SwaggerConfig.java
+++ backend/samals/src/main/java/com/project/samals/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.project.common.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.project.samals.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
