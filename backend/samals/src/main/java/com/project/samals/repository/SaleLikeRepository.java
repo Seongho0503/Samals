7a36c41a7bce27a6649b219e0bbef180e8168d67
Jaehwany <eejh22@gmail.com>
2022-09-23 11:16:48 +0900

    feat : 거래 좋아요 API 구조 설계 (#10)

---
+++ backend/samals/src/main/java/com/project/samals/repository/SaleLikeRepository.java
@@ -0,0 +1,9 @@
+package com.project.samals.repository;
+
+import com.project.samals.domain.SaleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleLikeRepository extends JpaRepository<SaleLike, Long> {
+
+
+}
