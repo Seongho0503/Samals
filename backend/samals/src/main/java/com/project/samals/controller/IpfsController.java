c07a936ef88967d30dc0145f8186ffca83b91830
Chung Lee <s2econd.blue@gmail.com>
2022-09-25 03:07:43 +0900

    feature : ipfs API 구현

--- backend/samals/src/main/java/com/project/samals/controller/IpfsController.java
+++ backend/samals/src/main/java/com/project/samals/controller/IpfsController.java
@@ -1,2 +1,53 @@
-package com.project.samals.controller;public class IpfsController {
+package com.project.samals.controller;
+
+import com.project.samals.domain.Ipfs;
+import com.project.samals.dto.IpfsDto;
+import com.project.samals.repository.IpfsRepository;
+import com.project.samals.service.IpfsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ipfs")
+@Api(tags={"Ipfs API"})
+public class IpfsController {
+    private  final IpfsRepository ipfsRepository;
+    private  final IpfsService ipfsService;
+    private static final Logger log = LoggerFactory.getLogger(IpfsController.class);
+
+    @ApiOperation(value="ipfs 데이터 추가")
+    @PostMapping("/add")
+    public ResponseEntity<Map> addIpfs(@RequestBody Map<String, Object> request){
+        return new ResponseEntity<>(ipfsService.addIpfs(request), HttpStatus.OK);
+    }
+
+    @ApiOperation(value="다음 ipfs 데이터 조회")
+    @GetMapping("/list")
+    public ResponseEntity<List<Ipfs>> getIpfsList(){
+        List<Ipfs> ipfsList = ipfsRepository.findAllBy();
+        log.info("ipfs -> {}", ipfsList.toArray().toString());
+
+        return new ResponseEntity<>(ipfsList, HttpStatus.OK);
+    }
+    @ApiOperation(value="다음 ipfs 데이터 조회")
+    @GetMapping("/peek")
+    public ResponseEntity<IpfsDto> peekIpfs(){
+        return new ResponseEntity<>(ipfsService.peekIpfs(), HttpStatus.OK);
+    }
+
+    @ApiOperation(value="다음 ipfs 데이터 사용처리 및 조회")
+    @GetMapping("/poll")
+    public ResponseEntity<IpfsDto> pollIpfs(){
+        return new ResponseEntity<>(ipfsService.pollIpfs(), HttpStatus.OK);
+    }
 }
--- backend/samals/src/main/java/com/project/samals/service/IpfsService.java
+++ backend/samals/src/main/java/com/project/samals/service/IpfsService.java
@@ -1,2 +1,86 @@
-package com.project.samals.service;public class IpfsService {
+package com.project.samals.service;
+
+import com.project.samals.domain.Animal;
+import com.project.samals.domain.Ipfs;
+import com.project.samals.dto.IpfsDto;
+import com.project.samals.repository.AnimalRepository;
+import com.project.samals.repository.IpfsRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class IpfsService {
+    private final IpfsRepository ipfsRepository;
+    private final AnimalRepository animalRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(IpfsService.class);
+
+    //ipfs 추가
+    public Map<String,Object> addIpfs(Map<String, Object> request){
+        Map<String,Object> result = new HashMap<>();
+
+        //필수 두 값중 하나가 null일 때
+        if(((String)request.get("ipfs_uri")).equals(null) || ((String)request.get("animal_species")).equals(null)){
+            result.put("status",false);
+            result.put("detail","null value is not possible");
+            return result;
+        }
+        else if(((String)request.get("ipfs_uri")).length() != 46){
+            result.put("status",false);
+            result.put("detail","ipfs_uri's size is not right");
+            return result;
+        }
+        //동물 검색
+        Animal animal = animalRepository.findByAnimalSpecies((String)request.get("animal_species"));
+
+        //해당 동물이 없으면 리턴
+        if(animal == null){
+            result.put("status",false);
+            result.put("detail","no animal data in [tb_animal]");
+            return result;
+        }
+//        animal.setAnimalSpecies((String)request.get("animal_species"));
+
+        IpfsDto ipfsDto = new IpfsDto();
+        ipfsDto.setIpfs_uri((String)request.get("ipfs_uri"));
+        ipfsDto.setAnimal(animal);
+
+        Ipfs ipfs = ipfsDto.toEntity();
+        try{
+            ipfsRepository.save(ipfs);
+            result.put("status",true);
+            result.put("detail",ipfs);
+        }catch (Exception e){
+            e.printStackTrace();
+            result.put("status",false);
+            result.put("detail","duplicate ipfs_uri error");
+        }
+       finally {
+            return result;
+        }
+    }
+    public IpfsDto peekIpfs(){
+        Ipfs ipfs = ipfsRepository.findTopByIpfsIsUsedIsOrderByIpfsSeq("N");
+        log.info("ipfs -> {}", ipfs);
+        IpfsDto ipfsDto = new IpfsDto().convert(ipfs);
+        log.info("ipfsDto -> {}", ipfsDto);
+        return ipfsDto;
+    }
+    public IpfsDto pollIpfs(){
+        Ipfs ipfs = ipfsRepository.findTopByIpfsIsUsedIsOrderByIpfsSeq("N");
+        ipfs.setIpfsIsUsed("Y");
+        log.info("ipfs -> {}", ipfs);
+        IpfsDto ipfsDto = new IpfsDto().convert(ipfs);
+        log.info("ipfsDto -> {}", ipfsDto);
+        return ipfsDto;
+    }
 }
