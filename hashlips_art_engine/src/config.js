c42199294f579a5c3c8aa633ea8ed39c7c124abc
Chung Lee <s2econd.blue@gmail.com>
2022-09-26 20:28:42 +0900

    feat : PFP 아이템 작업물

--- hashlips_art_engine/src/config.js
+++ hashlips_art_engine/src/config.js
@@ -5,9 +5,9 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace";
+const namePrefix = "SAVE THE ANIMALS";
+const description = "You can save the animals";
+const baseUri = "https://ipfs.io/ipfs/QmScDFHshmytW1P8Q1PqfPajNUzkbcNZMESCmER2QVFjwN";
 
 const solanaMetadata = {
   symbol: "YC",
