45eecc864b1ca69ec8854245d641c735ed2a6b0f
Jaehwany <eejh22@gmail.com>
2022-09-27 14:42:51 +0900

    feat : 동물 API 칼럼 추가 (#24)

--- backend/samals/src/main/java/com/project/samals/dto/AnimalDto.java
+++ backend/samals/src/main/java/com/project/samals/dto/AnimalDto.java
@@ -4,6 +4,8 @@ package com.project.samals.dto;
 import com.project.samals.domain.Animal;
 import lombok.*;
 
+import javax.persistence.Column;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,10 +14,26 @@ import lombok.*;
 @ToString
 public class AnimalDto {
     private String animal_species;
+
+    private String animalName;
     private String animal_class;
+
+    private String animalHabitat;
     private String animal_description;
     private int animal_total;
     private int animal_current;
+
+    private String animalImg1;
+
+    private String animalImg2;
+
+    private String animalImg3;
+
+    private String animalImg4;
+
+    private String animalImg5;
+
+    private String animalImg6;
 
     //객체를 관계형 데이터로 변환
     public Animal toEntity() {
@@ -35,8 +53,15 @@ public class AnimalDto {
                 .animal_species(animal.getAnimalSpecies())
                 .animal_class(animal.getAnimalClass())
                 .animal_description(animal.getAnimalDescription())
+                .animalHabitat(animal.getAnimalHabitat())
                 .animal_total(animal.getAnimalTotal())
                 .animal_current(animal.getAnimalCurrent())
+                .animalImg1(animal.getAnimalImg1())
+                .animalImg2(animal.getAnimalImg2())
+                .animalImg3(animal.getAnimalImg3())
+                .animalImg4(animal.getAnimalImg4())
+                .animalImg5(animal.getAnimalImg5())
+                .animalImg6(animal.getAnimalImg6())
                 .build();
     }
 }
