ff7a94fec8d4fa02e263baaa6b28212c466bc43f
kimchaeri <kcr7812@naver.com>
2022-09-28 16:44:20 +0900

    feat : 동물 api 연결 (#22)

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,21 +1,44 @@
 import axios from "axios";
 
-const BASE_URL = "http://j7d103.p.ssafy.io:8080/api";
+const BASE_URL =
+    "http://j7d103.p.ssafy.io:8080/api";
 //const BASE_URL = "http://j7d103.p.ssafy.io:8080/api/ipfs/list";
 
 export async function getDescription() {
-  try {
-    // const jwt = localStorage.getItem("jwt");
-    //${BASE_URL}
-    const response = await axios({
-      method: "GET",
-      url: `/api/shop/{animalSpecies}`,
-      //   headers: { Authorization: "Bearer " + jwt },
-    });
+    try {
+        // const jwt = localStorage.getItem("jwt");
+        //${BASE_URL}
+        const response =
+            await axios(
+                {
+                    method: "GET",
+                    url: `/api/shop/{animalSpecies}`,
+                    //   headers: { Authorization: "Bearer " + jwt },
+                }
+            );
 
-    console.log(response);
-    return response;
-  } catch {
-    return "";
-  }
+        console.log(
+            response
+        );
+        return response;
+    } catch {
+        return "";
+    }
+}
+
+export async function getAnimalData() {
+    try {
+        const response =
+            await axios(
+                {
+                    method: "GET",
+                    url: `/api/animal/list`,
+                    // headers :
+                }
+            );
+
+        return response;
+    } catch {
+        return "";
+    }
 }
