90f2f59053df7319a2b2efd3795915be00f87f7c
권성호 <maystar8956@naver.com>
2022-09-28 13:37:32 +0900

    feat : axios app.js 생성 (#23)

--- Client/src/pages/Explore.js
+++ Client/src/pages/Explore.js
@@ -5,7 +5,10 @@ import "../styles/Explore/Explore.css";
 import Search from "../components/Search";
 import MainLast from "../components/Main/MainLast";
 import Market from "../assets/market.png";
+import { getDescription } from "../api";
 const Explore = () => {
+  const response = getDescription();
+  console.log(response);
   return (
     <div id="explore">
       <img className="market-img" src={Market}></img>
