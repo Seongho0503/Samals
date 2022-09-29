87747038f3024af78ab50e6f599240b20bf8deb6
권성호 <maystar8956@naver.com>
2022-09-29 14:49:17 +0900

    fix : 거래소 api 파라미터 변경 (#31)

--- Client/src/api.js
+++ Client/src/api.js
@@ -3,36 +3,23 @@ import axios from "axios";
 const BASE_URL = "http://j7d103.p.ssafy.io:8080/api";
 //const BASE_URL = "http://j7d103.p.ssafy.io:8080/api/ipfs/list";
 
-<<<<<<< HEAD
 // 상점 :  동물 설명
 export async function getDescription(animalSpecies) {
-=======
-export async function getDescription() {
->>>>>>> 7d8be36333d8a6d86c6fbdf8e868ad1e288afcec
   try {
     // const jwt = localStorage.getItem("jwt");
     //${BASE_URL}
     const response = await axios({
       method: "GET",
-<<<<<<< HEAD
       url: `/api/shop/${animalSpecies}`,
       //   headers: { Authorization: "Bearer " + jwt },
     });
     // console.log(response);
-=======
-      url: `/api/shop/{animalSpecies}`,
-      //   headers: { Authorization: "Bearer " + jwt },
-    });
-
-    console.log(response);
->>>>>>> 7d8be36333d8a6d86c6fbdf8e868ad1e288afcec
     return response;
   } catch {
     return "";
   }
 }
 
-// 동물 리스트 조회
 export async function getAnimalData() {
   try {
     const response = await axios({
@@ -47,21 +34,11 @@ export async function getAnimalData() {
   }
 }
 
-<<<<<<< HEAD
 export async function getAnimalList() {
   try {
     const response = await axios({
       method: "GET",
-      url: `/api/sale/1/list`,
-=======
-// 기부 총액 조회
-export async function getTotalDonate() {
-  try {
-    const response = await axios({
-      method: "GET",
-      url: `/api/nft/total-donate`,
->>>>>>> 7d8be36333d8a6d86c6fbdf8e868ad1e288afcec
-      // headers :
+      url: `/api/sale/list`,
     });
 
     return response;
